755e5eddac260283ccdb001c050c75ae5d6b6359
jinyuhe <1144565385@qq.com>
2025-09-09 22:22:17 +0800

    优化WebGL示例HTML文件，调整代码格式，增强可读性，完善图像加载和纹理处理逻辑，确保功能正常运行

---
+++ js/index.js
@@ -0,0 +1,4 @@
+export { default as WebGL } from './webgl.js';
+export { default as Object3D } from './object3d.js';
+export { default as Camera } from './camera.js';
+export { default as Renderer } from './render.js';
